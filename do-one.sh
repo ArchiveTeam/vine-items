f11a4b85857dae36d9217c27e22838bcda8b9f3d
David Yip <yipdw@member.fsf.org>
2016-12-10 17:06:06 +0000

    Hide curl spam; move processed files to TARGET.

--- do-one.sh
+++ do-one.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-lua process.lua $1 | curl -u "$CREDENTIALS" -H 'Content-Type: application/json' -X POST -s --data-binary @- http://localhost:5984/vine/_bulk_docs
+echo $1
+lua process.lua $1 | curl -u "$CREDENTIALS" -H 'Content-Type: application/json' -X POST -s --data-binary @- http://localhost:5984/vine/_bulk_docs > /dev/null
+mv $1 $TARGET
