c45bd29d8c2a65a4aecf45cc2d1be827b52e2b21
David Yip <yipdw@member.fsf.org>
2016-12-10 17:05:48 +0000

    Add SOURCE and TARGET checks to do-many.sh.

--- do-many.sh
+++ do-many.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
+if [ ! -n "${SOURCE+x}" ]; then
+	echo '$SOURCE not set'
+	exit 1
+fi
+
+if [ ! -n "${TARGET+x}" ]; then
+	echo '$TARGET not set'
+	exit 1
+fi
+
 find $SOURCE -iname 'vine-*_data.txt' | parallel -j8 ./do-one.sh {}
